a11be0cbdf327d55ce6043a9331fdf3335632360
hxzmm <hxzmm@qq.com>
2018-08-13 04:45:21 +0800

    Create ssr.sh

---
+++ install/ssr.sh
@@ -0,0 +1,9 @@
+if [ ! -x python ]; then
+    apt install python
+fi
+apt-get install git
+git clone https://github.com/hxzmm/shadowsocksr.git
+cd ./shadowsocksr
+bash initcfg.sh
+wget -O /etc/init.d/shadowsocks https://raw.githubusercontent.com/hxzmm/shadowsocksr/manyuser/utils/shadowsocks 
+chmod 755 /etc/init.d/shadowsocks ; update-rc.d shadowsocks defaults ; service shadowsocks start
