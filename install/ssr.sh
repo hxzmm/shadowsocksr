ed39be8bf8267021e146df5a64c8be3fcfea6119
hxzmm <hxzmm@qq.com>
2018-08-13 04:48:01 +0800

    Update ssr.sh

--- install/ssr.sh
+++ install/ssr.sh
@@ -5,5 +5,5 @@ apt-get install git
 git clone https://github.com/hxzmm/shadowsocksr.git
 cd ./shadowsocksr
 bash initcfg.sh
-wget -O /etc/init.d/shadowsocks https://raw.githubusercontent.com/hxzmm/shadowsocksr/manyuser/utils/shadowsocks 
-chmod 755 /etc/init.d/shadowsocks ; update-rc.d shadowsocks defaults ; service shadowsocks start
+wget -O /etc/init.d/shadowsocks https://raw.githubusercontent.com/hxzmm/shadowsocksr/manyuser/install/shadowsocks 
+chmod 755 /etc/init.d/shadowsocks; update-rc.d shadowsocks defaults; service shadowsocks start
