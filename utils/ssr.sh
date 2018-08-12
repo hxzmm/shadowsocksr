7cd515bd48dd2a319df71f49e00c92de8d64a75b
hxzmm <hxzmm@qq.com>
2018-08-13 04:46:56 +0800

    Delete ssr.sh

--- utils/ssr.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -x python ]; then
-    apt install python
-fi
-apt-get install git
-git clone https://github.com/hxzmm/shadowsocksr.git
-cd ./shadowsocksr
-bash initcfg.sh
-wget -O /etc/init.d/shadowsocks https://raw.githubusercontent.com/hxzmm/shadowsocksr/manyuser/utils/shadowsocks 
-chmod 755 /etc/init.d/shadowsocks ; update-rc.d shadowsocks defaults ; service shadowsocks start
